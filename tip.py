1a5be891dbd797e358872a542548f74ca6f8a008
BrettPoindexter <brett@BrettsPC>
2023-10-11 07:22:09 -0400

    Added repeat choice // Clean up

--- tip.py
+++ tip.py
@@ -1,11 +1,17 @@
 while True:
-    bill = float(input("Bill Amount: $"))
+    bill = float(input("\nBill Amount: $"))
     percent = int(input("Tip Percentage: %"))
 
     tip = (percent / 100) * bill
     total = tip + bill
+
     print("\n----------------------------------")
     print("Bill Amount: $" + "%.2f" % bill)
     print(f"%{percent} of Bill: $" + "%.2f" % tip)
     print("Total: $" + "%.2f" % total)
     print("----------------------------------\n")
+
+    user = input("Continue? y/n: ")
+
+    if user == "n":
+        break
