84e3a56a0aee5f656260f4169de38904b2d14daf
Brett <poindexterbrett@gmail.com>
2023-10-11 10:59:54 -0400

    Created guessing game V.1

---
+++ guess.py
@@ -0,0 +1,25 @@
+#import random
+import random
+
+#Generate CPU number
+cpu = random.randint(1, 10)
+
+#Initialize guess counter
+count = 1
+
+#While loop for main game, defualt = 5 rounds
+while count <= 5:
+    #User input for user guess
+    user = int(input("Enter a number (1-10): "))
+    
+    #If statement for game logic
+    if user == cpu:
+        print(f"Congrats, you win! The number was {cpu}")
+        break
+    elif user != cpu:
+        print("Try again!")
+        count = count + 1
+
+#Losing message for loss event
+if count > 5:
+    print(f"Sorry, you lose! The number was {cpu}")
